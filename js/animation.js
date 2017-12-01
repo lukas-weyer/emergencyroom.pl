e331904e6e2a34597baa55f7ee04fa7736fd00ea
weyer-semenik <weyer.rm@gmail.com>
2017-12-01 13:54:26 +0100

    add some change to Pedi Calc beta

--- js/animation.js
+++ js/animation.js
@@ -1,1 +1,8 @@
-new WOW().init();
+wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: false, // default
+    live: true // default
+});
+wow.init();
